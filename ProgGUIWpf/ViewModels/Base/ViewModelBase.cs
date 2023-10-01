bdd4ef8a9e9ce5635a923bbf87a28f7e37e55042
yaroslavfed <yaroslav.site@mail.ru>
2023-10-01 22:25:16 +0700

    Переход от стандартного wpf шаблона к паттерну mvvm Добавлены необходимые директории Реализован родительский `ViewModelBase` класс для всех вью-моделей

---
+++ ProgGUIWpf/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProgGUIWpf.ViewModels.Base;
+
+internal abstract class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void InvokePropertyChanged([CallerMemberName] string PropertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+    }
+
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+    {
+        if(Equals(field, value))
+            return false;
+        field = value;
+        InvokePropertyChanged(PropertyName);
+        return true;
+    }
+}
