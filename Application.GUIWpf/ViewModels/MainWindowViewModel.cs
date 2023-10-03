9278bf0c9cf71258b82801f580a4957221037559
yaroslavfed <yaroslav.site@mail.ru>
2023-10-04 00:09:25 +0700

    Реализован функционал кнопок add/delete У списка файлов созданы кнопки для их добавления и удаления

--- Application.GUIWpf/ViewModels/MainWindowViewModel.cs
+++ Application.GUIWpf/ViewModels/MainWindowViewModel.cs
@@ -15,7 +15,7 @@ internal class MainWindowViewModel : ViewModelBase
 
     private string _title = "Главное окно";
 
-    private DataLocation _selectedFile;
+    private DataLocation _selectedFile = default!;
 
     #endregion
 
@@ -30,10 +30,14 @@ internal class MainWindowViewModel : ViewModelBase
     }
 
     #endregion
-    
+
     #region Commands
 
     public ICommand CloseApplicationCommand { get; }
+
+    public ICommand CreateNewFileCommand { get; }
+
+    public ICommand DeleteNewFileCommand { get; }
 
     #endregion
 
@@ -45,7 +49,7 @@ internal class MainWindowViewModel : ViewModelBase
         get => _title;
         set => Set(ref _title, value);
     }
-    
+
     #endregion
 
     #region Constructors
@@ -55,6 +59,10 @@ internal class MainWindowViewModel : ViewModelBase
         CloseApplicationCommand =
             new Command(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
+        CreateNewFileCommand = new Command(OnCreateNewFileCommandExecuted, CanCreateNewFileCommandExecute);
+
+        DeleteNewFileCommand = new Command(OnDeleteNewFileCommandExecuted, CanDeleteNewFileCommandExecute);
+
         // TODO: убрать сидирование, заменить на получение его из файла
         var indexSeedPoint = 1;
         var points = Enumerable.Range(1, 3).Select(i => new Location
@@ -62,13 +70,13 @@ internal class MainWindowViewModel : ViewModelBase
             PointX = indexSeedPoint,
             PointY = Math.Pow(indexSeedPoint++, 2)
         });
-        
+
         var dataTemplate = Enumerable.Range(1, 2).Select(i => new DataLocation
         {
             Namespace = $"File {i}",
             LocationsList = new ObservableCollection<Location>(points)
         });
-        
+
         DataLocations = new ObservableCollection<DataLocation>(dataTemplate);
     }
 
@@ -81,6 +89,42 @@ internal class MainWindowViewModel : ViewModelBase
     private void OnCloseApplicationCommandExecuted(object parameter) => System.Windows.Application.Current.Shutdown();
 
     private bool CanCloseApplicationCommandExecute(object parameter) => true;
+
+    #endregion
+
+    #region CreateNewFileCommandExecute
+
+    private void OnCreateNewFileCommandExecuted(object parameter)
+    {
+        var dataLocationIndex = DataLocations.Count + 1;
+        DataLocations.Add(
+            new DataLocation
+            {
+                Namespace = $"File {dataLocationIndex}",
+                LocationsList = new ObservableCollection<Location>()
+            });
+    }
+
+    private bool CanCreateNewFileCommandExecute(object parameter) => true;
+
+    #endregion
+
+    #region DeleteNewFileCommandExecute
+
+    private void OnDeleteNewFileCommandExecuted(object parameter)
+    {
+        if (parameter is not DataLocation dataLocation)
+            return;
+        var dataLocationIndex = DataLocations.IndexOf(dataLocation);
+        
+        DataLocations.Remove(dataLocation);
+        
+        if (dataLocationIndex < DataLocations.Count)
+            SelectedFile = DataLocations[dataLocationIndex];
+    }
+
+    private bool CanDeleteNewFileCommandExecute(object parameter) =>
+        parameter is DataLocation dataLocation && DataLocations.Contains(dataLocation);
 
     #endregion
 
